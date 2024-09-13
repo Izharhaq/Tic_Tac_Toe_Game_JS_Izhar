f9601cf4e9c42c46d5c467fe5685c9add410a269
izhar <ihansari2016@gmail.com>
2024-09-13 10:40:13 +0530

    added few logic in JS file and added a txt file for further addons features

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 let boxes = document.querySelectorAll(".box");
 let resetBtn = document.querySelector("#reset-btn");
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
 
 let turnO = true; //playerX, playerO
 
@@ -15,9 +18,16 @@ const winPatterns = [
 
 ];
 
+const resetGame = () => {
+    turnO = true;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+
+}
+
 boxes.forEach((box) => {
     box.addEventListener("click", ()=>{
-        console.log("box was clicked");
+        // console.log("box was clicked");
         if (turnO){
             box.innerText = "O";
             turnO = false;
@@ -28,5 +38,47 @@ boxes.forEach((box) => {
 
         }
         box.disabled = true;
-    })
-})
+
+        checkWinner();
+
+    });
+});
+
+const disableBoxes = () => {
+    for (let box of boxes) {
+        box.disabled = true;
+    }
+}
+
+const enableBoxes = () => {
+    for (let box of boxes) {
+        box.disabled = false;
+        box.innerText="";
+    }
+}
+
+const showWinner = (winner) => {
+    msg.innerText = `Congratulations, Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+
+}
+
+const checkWinner = () => {
+    for(let pattern of winPatterns) {
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+
+        if(pos1Val != "" && pos2Val != "" && pos3Val != "") {
+            if(pos1Val === pos2Val && pos2Val === pos3Val){
+                // console.log("winner", pos1Val);
+                showWinner(pos1Val);
+
+            }
+        }
+    }
+};
+
+newGameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
